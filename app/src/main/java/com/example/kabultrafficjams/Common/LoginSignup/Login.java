2c5a654bd25639eab5ac17c0c9fc7593e7a21028
Refa-Ghaznavi <refaghazanvi@gmail.com>
2020-06-27 16:56:02 +0430

    part 20 is finished part 22 is completed

--- app/src/main/java/com/example/kabultrafficjams/Common/LoginSignup/Login.java
+++ app/src/main/java/com/example/kabultrafficjams/Common/LoginSignup/Login.java
@@ -1,17 +1,123 @@
 package com.example.kabultrafficjams.Common.LoginSignup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.kabultrafficjams.R;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.hbb20.CountryCodePicker;
 
 public class Login extends AppCompatActivity {
+
+    // variables
+    CountryCodePicker countryCodePicker;
+    TextInputLayout phoneNumber, password;
+    RelativeLayout progressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_retailer_login);
+
+        // hooks
+        countryCodePicker = findViewById(R.id.login_country_code_picker);
+        phoneNumber = findViewById(R.id.login_phone_number);
+        password = findViewById(R.id.login_password);
+        progressbar = findViewById(R.id.login_progress_bar);
+
+    }
+
+
+    // login the user in app!
+    public void letTheUserLoggedIn(View view) {
+        // validate username and password
+        if (!validateFields()) {
+            return;
+        }
+
+        progressbar.setVisibility(View.VISIBLE);
+
+
+        // get the data
+        String _phoneNumber = phoneNumber.getEditText().getText().toString().trim();
+        final String _password = password.getEditText().getText().toString().trim();
+
+        if (_phoneNumber.charAt(0) == '0') {
+            _phoneNumber = _phoneNumber.substring(1);
+        }
+        final String _completePhoneNumber = "+" + countryCodePicker.getFullNumber() + _phoneNumber;
+
+        //Database
+        Query checkUser = FirebaseDatabase.getInstance().getReference("Users").orderByChild("phoneNo").equalTo(_completePhoneNumber);
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    phoneNumber.setError(null);
+                    phoneNumber.setErrorEnabled(false);
+
+                    String systemPassword = dataSnapshot.child(_completePhoneNumber).child("password").getValue(String.class);
+                    if (systemPassword.equals(_password)) {
+                        password.setError(null);
+                        password.setErrorEnabled(false);
+
+                        String _fullName = dataSnapshot.child(_completePhoneNumber).child("fullName").getValue(String.class);
+                        String _email = dataSnapshot.child(_completePhoneNumber).child("email").getValue(String.class);
+                        String _phoneNo = dataSnapshot.child(_completePhoneNumber).child("phoneNo").getValue(String.class);
+                        String _dateOfBirth = dataSnapshot.child(_completePhoneNumber).child("date").getValue(String.class);
+
+                        Toast.makeText(Login.this,_fullName+"\n"+_email+"\n"+_phoneNo+"\n"+_dateOfBirth, Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        progressbar.setVisibility(View.GONE);
+                        Toast.makeText(Login.this, "Password does not match!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    progressbar.setVisibility(View.GONE);
+                    Toast.makeText(Login.this, "No such user exist!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                progressbar.setVisibility(View.GONE);
+                Toast.makeText(Login.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private boolean validateFields() {
+
+        String _phoneNumber = phoneNumber.getEditText().getText().toString().trim();
+        String _password = password.getEditText().getText().toString().trim();
+
+        if (_phoneNumber.isEmpty()) {
+            phoneNumber.setError("Phone number can not be empty");
+            phoneNumber.requestFocus();
+            return false;
+        } else if (_password.isEmpty()) {
+            password.setError("Password can not be empty");
+            password.requestFocus();
+            return false;
+        } else {
+            // check the video
+            phoneNumber.setError(null);
+            password.setError(null);
+            return true;
+        }
     }
 }
